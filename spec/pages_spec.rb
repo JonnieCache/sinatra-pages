c3a73a5e066670a2fd09fd81edd9f837b74f8d0c
Julio Javier Cicchelli <javier@rock-n-code.com>
2010-01-15 05:56:57 +0100

    Defined the generic route '/:page' scenario.

--- spec/pages_spec.rb
+++ spec/pages_spec.rb
@@ -10,6 +10,9 @@ describe Sinatra::Pages do
   before :all do
     FileUtils.mkdir 'views'
     File.open('views/home.haml', 'w'){|file| file << 'Home'}
+    File.open('views/generic.haml', 'w'){|file| file << 'Generic'}
+    File.open('views/generic_test.haml', 'w'){|file| file << 'Generic Test'}
+    File.open('views/another_generic_test.haml', 'w'){|file| file << 'Another Generic Test'}
   end
   
   context "when using the HTTP GET request method" do
@@ -21,6 +24,19 @@ describe Sinatra::Pages do
 
         last_response.should be_ok
         last_response.body.chomp.should == 'Home'
+      end
+    end
+    
+    it "should render an existing page if the given route match the '/:page' pattern." do
+      ['Generic', 'Generic Test', 'Another Generic Test'].each do |file|
+        filename = file.downcase.gsub(' ', '_')
+        
+        File.exist?("views/#{filename}.haml").should be_true
+
+        get "/#{filename}"
+        
+        last_response.should be_ok
+        last_response.body.chomp.should == file
       end
     end
   end
