5c4891222d4f0c473c24a2c76cce68dd08a861f1
Julio Javier Cicchelli <javier@rock-n-code.com>
2010-01-15 04:11:15 +0100

    Defined the empty route scenario.

--- spec/pages_spec.rb
+++ spec/pages_spec.rb
@@ -13,7 +13,17 @@ describe Sinatra::Pages do
   end
   
   context "when using the HTTP GET request method" do
-    it "should render the Home page if the given route is '/'" do
+    it "should render the Home page if the given route is empty." do
+      File.exist?('views/home.haml').should be_true
+      
+      get ''
+      
+      p last_response
+      last_response.should be_ok
+      last_response.body.chomp.should == 'Home'
+    end
+    
+    it "should render the Home page if the given route is '/'." do
       File.exist?('views/home.haml').should be_true
       
       get '/'
