c1149afed660df5eedc9830b89dda5c597d81a38
Julio Javier Cicchelli <javier@rock-n-code.com>
2010-01-17 18:08:20 +0100

    Added the generic route '/*/:page' for the HTTP GET request method.

--- lib/sinatra/pages.rb
+++ lib/sinatra/pages.rb
@@ -3,15 +3,25 @@ require 'haml'
 
 module Sinatra
   class Pages < Sinatra::Base
-    %w[/? /:page].each do |route|
-      get route do
-        params[:page] = 'home' if params[:page].nil?
-        
-        begin
-          haml params[:page].to_sym
-        rescue Errno::ENOENT
-          halt 404
-        end
+    get '/?' do
+      params[:page] = 'home'
+      
+      haml :home
+    end
+    
+    get '/:page' do
+      begin
+        haml params[:page].to_sym
+      rescue Errno::ENOENT
+        halt 404
+      end
+    end
+    
+    get '/*/:page' do
+      begin
+        haml "#{params[:splat].first}/#{params[:page]}".to_sym
+      rescue Errno::ENOENT
+        halt 404
       end
     end
     
