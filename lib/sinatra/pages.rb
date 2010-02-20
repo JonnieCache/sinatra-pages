48b493f2106604f70b050a21b76eb0c7c2f2b587
Julio Javier Cicchelli <javier@rock-n-code.com>
2010-02-20 16:34:59 +0100

    Rewritten the code in a Sinatra extension fashion.

--- lib/sinatra/pages.rb
+++ lib/sinatra/pages.rb
@@ -2,26 +2,30 @@ require 'sinatra/base'
 require 'haml'
 
 module Sinatra
-  class Pages < Sinatra::Base
-    %w[/? /:page /*/:page].each do |route|
-      get route do
-        params[:page] = 'home' if params[:page].nil?
-        
-        page_to_render = params[:splat].nil? ? '' : "#{params[:splat].first}/"
-        page_to_render << params[:page]
-        
-        begin
-          haml page_to_render.to_sym, :layout => !request.xhr?
-        rescue Errno::ENOENT
-          halt 404
+  module Pages
+    def self.registered(app)
+      %w[/? /:page /*/:page].each do |route|
+        app.get route do
+          params[:page] = 'home' if params[:page].nil?
+
+          page_to_render = params[:splat].nil? ? '' : "#{params[:splat].first}/"
+          page_to_render << params[:page]
+
+          begin
+            haml page_to_render.to_sym, :layout => !request.xhr?
+          rescue Errno::ENOENT
+            halt 404
+          end
         end
       end
-    end
-    
-    not_found do
-      params[:page] = 'not_found'
-      
-      haml :not_found, :layout => !request.xhr?
+
+      app.not_found do
+        params[:page] = 'not_found'
+
+        haml :not_found, :layout => !request.xhr?
+      end
     end
   end
+  
+  register Pages
 end
